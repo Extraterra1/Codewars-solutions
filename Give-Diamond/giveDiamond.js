99cabaca1b78d72b95494136052d4e51b30011c6
Carlos Pires <extraterra1@gmail.com>
2023-08-24 10:16:39 +0100

    sol: Give-Diamond

---
+++ Give-Diamond/giveDiamond.js
@@ -0,0 +1,26 @@
+// Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
+
+// Task
+// You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+
+// Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
+
+// Examples
+// A size 3 diamond:
+
+//  *
+// ***
+//  *
+
+// ...which would appear as a string of " *\n***\n *\n"
+
+function diamond(n) {
+  if (n % 2 < 1) return null;
+  const rows = [];
+  for (let i = n; i > 0; i -= 2) {
+    rows.push(" ".repeat((n - i) / 2) + "*".repeat(i) + "\n");
+  }
+  return rows.slice(1).reverse().join("") + rows.join("");
+}
+
+console.log(diamond(17));
