5769af047d59d1cc65e1bf7f04e150547fc7fe55
Carlos Pires <extraterra1@gmail.com>
2023-11-11 17:47:18 +0000

    solve Unique-In-Order

--- Unique-In-Order/uniqueInOrder.js
+++ Unique-In-Order/uniqueInOrder.js
@@ -1,8 +1,8 @@
-const uniqueInOrder = (arr) => {
-  let prev;
-  return arr.map((e) => e);
+const uniqueInOrder = (input) => {
+  if (typeof input === 'string') input = input.split('');
+  return input.filter((e, i) => input[i + 1] !== e);
 };
 
-const test = uniqueInOrder('AAAABBBCCDAABBB');
+const test = uniqueInOrder([1, 2, 2, 3, 3]);
 
 console.log(test);
