b523ca85a7e2e3201e68443c113b0837c4c773f3
Carlos Pires <extraterra1@gmail.com>
2023-11-12 21:52:45 +0000

    solve Zeros-To-End

--- Zeros-To-End/zerosToEnd.js
+++ Zeros-To-End/zerosToEnd.js
@@ -1,4 +1,14 @@
-const moveZeros = (arr) => {};
+const moveZeros = (arr) => {
+  const zeroArr = [];
+  const numArr = arr.filter((e) => {
+    if (e === 0) {
+      zeroArr.push(e);
+      return false;
+    }
+    return true;
+  });
+  return numArr.concat(zeroArr);
+};
 
 const test = moveZeros([false, 1, 0, 1, 2, 0, 1, 3, 'a']);
 
