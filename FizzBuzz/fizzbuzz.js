e6aa630adf1efd3812403a5365d0cdc43ae1a623
Carlos Pires <extraterra1@gmail.com>
2023-11-24 13:46:37 +0000

    fizzbuz comment

---
+++ FizzBuzz/fizzbuzz.js
@@ -0,0 +1,9 @@
+// Write a function that takes a number as an argument and returns an array containing numbers from 1 to the given number.
+// But for multiples of 3, replace the number with "Fizz," for multiples of 5, replace with "Buzz," and for numbers which are multiples of both 3 and 5, replace with "FizzBuzz."
+
+// For example, if the input is 15, the output should be:
+// [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz'];
+
+const fizzBuzz = (num) => {};
+
+console.log(fizzBuzz(num));
