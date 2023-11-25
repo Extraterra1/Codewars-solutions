ccc363ce643a074ec2789eb1d6bccbb44a6b10bf
Carlos Pires <extraterra1@gmail.com>
2023-11-25 14:02:30 +0000

    solve ispalindrome

--- Is-Palindrome/isPalindrome.js
+++ Is-Palindrome/isPalindrome.js
@@ -8,3 +8,18 @@
 // isPalindrome('Hello, World!') should return false.
 // isPalindrome('A man, a plan, a canal, Panama!') should return true.
 // Try to implement this in 10 minutes or less! Good luck!
+
+const isPalindrome = (str) => {
+  const cleanStr = str.replace(/[\s\p{P}]/gu, '').toLowerCase();
+  const reverse = cleanStr.split('').reverse().join('');
+  return reverse === cleanStr;
+};
+
+console.log(isPalindrome('bob!')); // true (ignoring spaces and case)
+console.log(isPalindrome('Able , was I saw eLba')); // true (ignoring spaces and case)
+
+console.log(isPalindrome('radar')); // true
+console.log(isPalindrome('12321')); // true
+console.log(isPalindrome('race car')); // true (ignoring spaces)
+console.log(isPalindrome('hello')); // false
+console.log(isPalindrome('Was it a car or a cat I saw?')); // true (ignoring spaces and punctuation)
