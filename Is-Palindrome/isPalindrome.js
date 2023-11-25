2bef9b71d57c1eb16aab22b673f9da2494acb6ec
Carlos Pires <extraterra1@gmail.com>
2023-11-25 13:53:56 +0000

    isPalindrome comment

---
+++ Is-Palindrome/isPalindrome.js
@@ -0,0 +1,10 @@
+// Challenge: Palindrome Checker
+
+// Write a function that takes a string as an argument and returns true if the string is a palindrome and false otherwise. A palindrome is a word, phrase, number, or other sequence of characters that reads the same backward as forward, ignoring spaces, punctuation, and capitalization.
+
+// For example:
+
+// isPalindrome('level') should return true.
+// isPalindrome('Hello, World!') should return false.
+// isPalindrome('A man, a plan, a canal, Panama!') should return true.
+// Try to implement this in 10 minutes or less! Good luck!
