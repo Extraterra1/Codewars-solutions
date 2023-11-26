3953f1e4f4da1a4e57329ea449f551d07d95a36c
Carlos Pires <extraterra1@gmail.com>
2023-11-26 11:47:33 +0000

    solve areAnagrams

--- Are-Anagrams/areAnagrams.js
+++ Are-Anagrams/areAnagrams.js
@@ -8,3 +8,29 @@
 // areAnagrams('listen', 'silent') should return true.
 // areAnagrams('hello', 'world') should return false.
 // areAnagrams('The Morse Code!', 'Here come dots') should return true (ignoring spaces and case).
+
+const areAnagrams = (str1, str2) => {
+  const cleanStr1 = str1
+    .replace(/[^\w]|_/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+  const cleanStr2 = str2
+    .replace(/[^\w]|_/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+
+  return cleanStr1 === cleanStr2;
+};
+
+console.log(areAnagrams('listen', 'silent'));
+
+console.log(areAnagrams('evil', 'vile')); // true
+console.log(areAnagrams('astronomer', 'moon starer')); // true
+console.log(areAnagrams('school master', 'the classroom')); // true
+console.log(areAnagrams('Debit card', 'Bad credit')); // true (ignoring spaces and case)
+console.log(areAnagrams('test', 'best')); // false
+console.log(areAnagrams('funeral', 'real fun')); // false
