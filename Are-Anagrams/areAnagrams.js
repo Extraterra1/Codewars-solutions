5f1bcecb5fb947d92daa579927da7564ac54b38b
Carlos Pires <extraterra1@gmail.com>
2023-11-26 11:39:39 +0000

    areAnagrams comment

---
+++ Are-Anagrams/areAnagrams.js
@@ -0,0 +1,10 @@
+// Challenge: Anagram Checker
+
+// Write a function that takes two strings as arguments and returns true if the strings are anagrams of each other, and false otherwise.
+// Anagrams are words or phrases that contain the same characters, with the same or different order.
+
+// For example:
+
+// areAnagrams('listen', 'silent') should return true.
+// areAnagrams('hello', 'world') should return false.
+// areAnagrams('The Morse Code!', 'Here come dots') should return true (ignoring spaces and case).
