63d5ad6fd19d9cd8e6e4001171a573a3864e9deb
Carlos Pires <extraterra1@gmail.com>
2023-11-28 17:23:37 +0000

    solve countVowels

--- Count-Vowels/countVowels.js
+++ Count-Vowels/countVowels.js
@@ -6,4 +6,18 @@
 // For example:
 
 // countVowels('Hello World') should return 3 (o, o, e).
-// countVowels('Programming is Fun!') should return 6 (o, a, i, i, u, !).
+// countVowels('Programming is Fun!') should return 5 (o, a, i, i, u).
+
+const countVowels = (str) => {
+  return str.split('').reduce((acc, val) => (val.match(/[aeiouAEIOU]/g) ? acc + 1 : acc), 0);
+};
+
+console.log(countVowels('Hello World'));
+console.log(countVowels('Programming is Fun!'));
+console.log(countVowels('JavaScript')); // 3 (a, a, e)
+console.log(countVowels('Elephant')); // 3 (e, e, a)
+console.log(countVowels('Quick brown fox jumps over the lazy dog')); // 10
+console.log(countVowels('OpenAI GPT')); // 4 (o, e, A, I)
+console.log(countVowels('aeiouAEIOU')); // 10 (all vowels in both cases)
+console.log(countVowels('12345')); // 0 (no vowels)
+console.log(countVowels('!@#$%^&*()')); // 0 (no vowels)
