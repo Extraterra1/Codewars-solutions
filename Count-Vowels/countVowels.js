c8b2b0ee99c6de83221a12e2a0edb55157028877
Carlos Pires <extraterra1@gmail.com>
2023-11-28 17:12:40 +0000

    countVowels comment

---
+++ Count-Vowels/countVowels.js
@@ -0,0 +1,9 @@
+// Challenge: Counting Vowels
+
+// Write a function that takes a string as an argument and returns the count of vowels (a, e, i, o, u) in the string.
+// The function should be case-insensitive, meaning it should count both uppercase and lowercase vowels.
+
+// For example:
+
+// countVowels('Hello World') should return 3 (o, o, e).
+// countVowels('Programming is Fun!') should return 6 (o, a, i, i, u, !).
