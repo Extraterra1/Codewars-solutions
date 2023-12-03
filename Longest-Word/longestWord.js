3bb1b14772f7bdbec2baa67e9d98a90ca12f77bf
Carlos Pires <extraterra1@gmail.com>
2023-12-03 19:43:16 +0000

    solve longestWord

--- Longest-Word/longestWord.js
+++ Longest-Word/longestWord.js
@@ -6,3 +6,14 @@
 // longestWord('This is an example sentence') should return 'example'.
 // longestWord('Web development is fun!') should return 'development'.
 // longestWord('Hello world') should return 'world'.
+
+const longestWord = (word) => {
+  return word.split(' ').reduce((acc, val) => (val.length > acc.length ? val : acc));
+};
+
+console.log(longestWord('The quick brown fox jumps over the lazy dog')); // 'quick'
+console.log(longestWord('A journey of a thousand miles begins with a single step')); // 'journey'
+console.log(longestWord('Programming is an art')); // 'Programming'
+console.log(longestWord('Python, JavaScript, and Ruby are programming languages')); // 'JavaScript' (first occurrence of the longest word)
+console.log(longestWord('The longest word in the English language is pneumonoultramicroscopicsilicovolcanoconiosis')); // 'pneumonoultramicroscopicsilicovolcanoconiosis'
+console.log(longestWord('')); // '' (empty string)
