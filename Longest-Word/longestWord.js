292b9e319034783d7e63f3e3fcc32429a197cb07
Carlos Pires <extraterra1@gmail.com>
2023-12-03 19:38:11 +0000

    longestWord.js comment

---
+++ Longest-Word/longestWord.js
@@ -0,0 +1,8 @@
+// Write a function that takes a sentence (string) as an argument and returns the longest word in the sentence.
+// If there are multiple words with the same maximum length, return the first one.
+
+// For example:
+
+// longestWord('This is an example sentence') should return 'example'.
+// longestWord('Web development is fun!') should return 'development'.
+// longestWord('Hello world') should return 'world'.
