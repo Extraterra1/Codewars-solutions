8bcb88c4994e85d2b493ab136afee91a373513b6
Carlos Pires <extraterra1@gmail.com>
2024-07-02 12:28:26 +0100

    solve your order

--- Your-Order-Please/yourOrder.js
+++ Your-Order-Please/yourOrder.js
@@ -9,7 +9,15 @@
 // "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
 // ""  -->  ""
 
-const order = (words) => {};
+const order = (words) => {
+  return words
+    .split(' ')
+    .reduce((acc, val) => {
+      acc[val.match(/\d/gm) - 1] = val;
+      return acc;
+    }, [])
+    .join(' ');
+};
 
 console.log(order('is2 Thi1s T4est 3a'));
 console.log(order(''));
