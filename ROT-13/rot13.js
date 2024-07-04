e106cf0a6bfb73cb97f2317099d3af579b74099c
Carlos Pires <extraterra1@gmail.com>
2024-07-04 15:42:04 +0100

    solve rot13

--- ROT-13/rot13.js
+++ ROT-13/rot13.js
@@ -2,7 +2,23 @@
 
 // Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
 
-const rot13 = (str) => {};
+const rot13 = (str) => {
+  return str
+    .split('')
+    .map((e) => {
+      if (/[^a-zA-Z]+/.test(e)) return e;
+
+      const ogCode = e.charCodeAt();
+      const firstCode = e.toUpperCase() === e ? 65 : 97;
+
+      if (ogCode >= firstCode) {
+        const newCode = ogCode + 13;
+        if (newCode <= firstCode + 25) return String.fromCharCode(newCode);
+        return String.fromCharCode(firstCode + (newCode - (firstCode + 26)));
+      }
+    })
+    .join('');
+};
 
 console.log(rot13('test')); /* grfg */
 console.log(rot13('Test')); /* Grfg */
