fc89eb2fa184f964225c2e9d2ed1123c121300c1
Carlos Pires <extraterra1@gmail.com>
2024-07-05 10:35:07 +0100

    bouncing ball examples

--- Bouncing-Balls/bouncingBalls.js
+++ Bouncing-Balls/bouncingBalls.js
@@ -23,3 +23,7 @@
 // (Condition 2) not fulfilled).
 
 const bouncingBall = (h, bounce, window) => {};
+
+console.log(bouncingBall(3.0, 0.66, 1.5));
+console.log(bouncingBall(30.0, 0.66, 1.5));
+console.log(bouncingBall(3.0, 1.0, 1.5));
