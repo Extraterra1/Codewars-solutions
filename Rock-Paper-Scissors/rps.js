ac86d2c65b6d1b93b4f53b744daf4629203e35b0
Carlos Pires <extraterra1@gmail.com>
2024-07-06 16:53:18 +0100

    solve rps

--- Rock-Paper-Scissors/rps.js
+++ Rock-Paper-Scissors/rps.js
@@ -6,7 +6,13 @@
 // "scissors", "rock" --> "Player 2 won!"
 // "paper", "paper" --> "Draw!"
 
-const rps = (p1, p2) => {};
+const rps = (p1, p2) => {
+  if (p1 === p2) return 'Draw!';
+  if (p1 === 'rock' && p2 !== 'paper') return 'Player 1 won!';
+  if (p1 === 'paper' && p2 !== 'scissors') return 'Player 1 won!';
+  if (p1 === 'scissors' && p2 !== 'rock') return 'Player 1 won!';
+  return 'Player 2 won!';
+};
 
 console.log(rps('rock', 'scissors'));
 console.log(rps('scissors', 'paper'));
