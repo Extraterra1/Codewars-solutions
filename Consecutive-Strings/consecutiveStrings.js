df2b240340eebc8cfd980be26157092335b4ec40
Carlos Pires <extraterra1@gmail.com>
2024-07-06 12:47:37 +0100

    consecutive strings examples

--- Consecutive-Strings/consecutiveStrings.js
+++ Consecutive-Strings/consecutiveStrings.js
@@ -20,3 +20,15 @@
 // n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
 
 const longestConsec = (arr, k) => {};
+
+console.log(longestConsec(['tree', 'foling', 'trashy', 'blue', 'abcdef', 'uvwxyz'], 2));
+// console.log(longestConsec(['tree', 'foling', 'trashy', 'blue', 'abcdef', 'uvwxyz'], 3));
+console.log(longestConsec(['zone', 'abigail', 'theta', 'form', 'libe', 'zas'], 2));
+console.log(longestConsec(['ejjjjmmtthh', 'zxxuueeg', 'aanlljrrrxx', 'dqqqaaabbb', 'oocccffuucccjjjkkkjyyyeehh'], 1));
+console.log(longestConsec([], 3));
+console.log(longestConsec(['itvayloxrp', 'wkppqsztdkmvcuwvereiupccauycnjutlv', 'vweqilsfytihvrzlaodfixoyxvyuyvgpck'], 2));
+console.log(longestConsec(['wlwsasphmxx', 'owiaxujylentrklctozmymu', 'wpgozvxxiu'], 2));
+console.log(longestConsec(['zone', 'abigail', 'theta', 'form', 'libe', 'zas'], -2));
+console.log(longestConsec(['it', 'wkppv', 'ixoyx', '3452', 'zzzzzzzzzzzz'], 3));
+console.log(longestConsec(['it', 'wkppv', 'ixoyx', '3452', 'zzzzzzzzzzzz'], 15));
+console.log(longestConsec(['it', 'wkppv', 'ixoyx', '3452', 'zzzzzzzzzzzz'], 0));
