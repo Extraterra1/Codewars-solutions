3c85f7f5fa796c65d14cfd4db12154afef8b41f8
Carlos Pires <extraterra1@gmail.com>
2024-07-11 12:44:15 +0100

    binary addition examples

--- Disembowel-Trolls/Binary-Addtion/binaryAddition.js
+++ Disembowel-Trolls/Binary-Addtion/binaryAddition.js
@@ -8,3 +8,9 @@
 // 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
 
 const addBinary = (a, b) => {};
+
+console.log(addBinary(1, 1));
+console.log(addBinary(1, 5));
+console.log(addBinary(1, 54));
+console.log(addBinary(1, 321));
+console.log(addBinary(1, 5435436));
