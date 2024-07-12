30a30d2718b6be60f24e9cdedc531da3bdfc5556
Carlos Pires <extraterra1@gmail.com>
2024-07-12 21:25:12 +0100

    solve moving zeroes

--- Moving-Zeroes-to-End/movingZeroes.js
+++ Moving-Zeroes-to-End/movingZeroes.js
@@ -2,7 +2,13 @@
 
 // moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 
-const moveZeroes = (arr) => {};
+const moveZeroes = (arr) => {
+  return arr.reduce((acc, val) => {
+    if (val === 0) return [...acc, val];
+    const idx = acc.indexOf(0);
+    return idx !== -1 ? [...acc.slice(0, idx >= 0 ? idx : acc.length), val, ...acc.slice(idx)] : [...acc, val];
+  }, []);
+};
 
 console.log(moveZeroes([false, 1, 0, 1, 2, 0, 1, 3, 'a']));
 console.log(moveZeroes([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
