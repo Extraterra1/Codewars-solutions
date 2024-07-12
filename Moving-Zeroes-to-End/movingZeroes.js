7d2e6e2eb6cf4815bbf5e033952a9db5c4011cbc
Carlos Pires <extraterra1@gmail.com>
2024-07-12 21:24:58 +0100

    moving zeroes examples

--- Moving-Zeroes-to-End/movingZeroes.js
+++ Moving-Zeroes-to-End/movingZeroes.js
@@ -3,3 +3,6 @@
 // moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 
 const moveZeroes = (arr) => {};
+
+console.log(moveZeroes([false, 1, 0, 1, 2, 0, 1, 3, 'a']));
+console.log(moveZeroes([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
