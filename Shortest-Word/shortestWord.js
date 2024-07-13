789caa9aba70101367f5735b2be23670dcfcd10f
Carlos Pires <extraterra1@gmail.com>
2024-07-13 23:40:34 +0100

    shortest word examples

--- Shortest-Word/shortestWord.js
+++ Shortest-Word/shortestWord.js
@@ -3,3 +3,7 @@
 // String will never be empty and you do not need to account for different data types.
 
 const findShort = (s) => {};
+
+console.log(findShort('bitcoin take over the world maybe who knows perhaps'));
+console.log(findShort('turns out random test cases are easier than writing out basic ones'));
+console.log(findShort("Let's travel abroad shall we"));
