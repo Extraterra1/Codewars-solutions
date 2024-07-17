4a90e9c70f6561d56c04d0dc0511e3e3b73f8722
Carlos Pires <extraterra1@gmail.com>
2024-07-17 13:30:54 +0100

    not very secure examples

--- Not-Very-Secure/notSecure.js
+++ Not-Very-Secure/notSecure.js
@@ -6,9 +6,12 @@
 // Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
 // No whitespaces / underscore
 
-const alphanumeric = (str) => {};
+const alphanumeric = (str) => {
+  return !/[^a-zA-Z0-9]/.test(str);
+};
 
 console.log(alphanumeric('Mazinkaiser'));
 console.log(alphanumeric('"hello world_"'));
 console.log(alphanumeric('PassW0rd'));
 console.log(alphanumeric('                '));
+console.log(alphanumeric(''));
