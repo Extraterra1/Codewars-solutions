67402974be31f9e96363336f00ffc233c4625ff6
Carlos Pires <extraterra1@gmail.com>
2024-07-19 10:31:38 +0100

    shift explanation

--- Shift-Function/shiftFunction.js
+++ Shift-Function/shiftFunction.js
@@ -1,1 +1,35 @@
+// Been a while, but here's part 2!
+
+// You are given a string of lowercase letters and spaces that you need to type out. In the string there is a special function: [shift]. Once you encounter a [shift] , you capitalise the character right after it, as if you're actually holding the key. Return the final string .
+
+// e.g. [shift]john [shift]green return John Green (capitalise the j and g)
+
+// Walkthrough:
+
+// [shift]
+// J capitalise the j
+// Jo
+// Joh
+// John
+// John[space]
+// John G capitalise the g
+// John Gr
+// John Gre
+// John Gree
+// John Green
+
+// John Green
+
+// e.g. [shift]n[shift]o[shift]o[shift]o return NOOO (capitalise all the letters)
+
+// Walkthrough:
+
+// [shift]
+// N capitalise the n
+// NO capitalise the O
+// NOO capitalise the O
+// NOOO capitalise the O
+
+// NOOO
+
 const typeOut = (str) => {};
