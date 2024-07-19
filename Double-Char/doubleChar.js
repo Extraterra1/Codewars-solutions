431ee9f930c57f1f67d64923cff340896e6efb5d
Carlos Pires <extraterra1@gmail.com>
2024-07-19 10:26:32 +0100

    double char examples

--- Double-Char/doubleChar.js
+++ Double-Char/doubleChar.js
@@ -7,3 +7,11 @@
 // Good Luck!
 
 const doubleChar = (str) => {};
+
+console.log(doubleChar('String'));
+console.log(doubleChar('abcd'));
+console.log(doubleChar('Adidas'));
+console.log(doubleChar('1337'));
+console.log(doubleChar('illuminati'));
+console.log(doubleChar('123456'));
+console.log(doubleChar('%^&*('));
