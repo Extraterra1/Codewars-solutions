23a8ee3f4a73e0346db72d7cf6ca2bcb1a9675bb
Carlos Pires <extraterra1@gmail.com>
2024-07-20 15:49:21 +0100

    solve isLetter

--- Is-Letter/isLetter.js
+++ Is-Letter/isLetter.js
@@ -1,3 +1,10 @@
 // Write a function, isItLetter or is_it_letter or IsItLetter, which tells us if a given character is a letter or not.
 
-const isLetter = (char) => {};
+const isLetter = (char) => {
+  return /[a-zA-Z]/.test(char);
+};
+
+console.log(isLetter('a'));
+console.log(isLetter('2'));
+console.log(isLetter(''));
+console.log(isLetter('A'));
