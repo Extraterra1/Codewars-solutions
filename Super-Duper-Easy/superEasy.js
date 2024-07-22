17799e79bee0650bb0917bd0a876629feec768eb
Carlos Pires <extraterra1@gmail.com>
2024-07-22 11:43:00 +0100

    super easy examples

--- Super-Duper-Easy/superEasy.js
+++ Super-Duper-Easy/superEasy.js
@@ -1,3 +1,6 @@
 // Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
 
 const problem = (x) => {};
+
+console.log(problem(5));
+console.log(problem('heyy'));
