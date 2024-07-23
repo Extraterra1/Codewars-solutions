660575ac24aeddce3bcf3accca6f08d19b9d9fbe
Carlos Pires <extraterra1@gmail.com>
2024-07-23 11:38:37 +0100

    solve n divisible xy

--- N-Divisible-XY/isDivisible.js
+++ N-Divisible-XY/isDivisible.js
@@ -6,7 +6,9 @@
 // 3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
 // 4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
 
-const isDivisible = (n, x, y) => {};
+const isDivisible = (n, x, y) => {
+  return !(n % x) && !(n % y);
+};
 
 console.log(isDivisible(3, 3, 4));
 console.log(isDivisible(12, 3, 4));
