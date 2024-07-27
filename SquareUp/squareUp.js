984a1b8edbfeb2f6fba398e20af984528ab4d4bd
Carlos Pires <extraterra1@gmail.com>
2024-07-27 14:13:13 +0100

    solve squareUp

--- SquareUp/squareUp.js
+++ SquareUp/squareUp.js
@@ -7,7 +7,15 @@
 // squareUp(4) => [0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1]
 // 0 <= n <= 1000.
 
-const squareUp = (n) => {};
+const squareUp = (n) => {
+  let arr = [];
+  for (let i = 1; i <= n; i++) {
+    for (let j = n; j >= 1; j--) {
+      arr.push(j <= i ? j : 0);
+    }
+  }
+  return arr;
+};
 
 console.log(squareUp(2));
 console.log(squareUp(3));
