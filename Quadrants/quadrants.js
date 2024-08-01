8668ddda4ba6aeed4788c32d2a513510ec8fdea2
Carlos Pires <extraterra1@gmail.com>
2024-08-01 11:00:08 +0100

    solve quadrants

--- Quadrants/quadrants.js
+++ Quadrants/quadrants.js
@@ -7,7 +7,12 @@
 // (-1, -9)   => 3
 // (19, -56)  => 4
 
-const quadrant = (x, y) => {};
+const quadrant = (x, y) => {
+  if (x > 0 && y > 0) return 1;
+  if (x < 0 && y > 0) return 2;
+  if (x < 0 && y < 0) return 3;
+  if (x > 0 && y < 0) return 4;
+};
 
 console.log(quadrant(1, 2));
 console.log(quadrant(3, 5));
