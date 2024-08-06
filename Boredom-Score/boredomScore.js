3603b0e0283c6e1a7c13e82af5e58a8f72a611bb
Carlos Pires <extraterra1@gmail.com>
2024-08-06 11:39:50 +0100

    boredom score explanation

--- Boredom-Score/boredomScore.js
+++ Boredom-Score/boredomScore.js
@@ -1,1 +1,18 @@
+// Every now and then people in the office moves teams or departments. Depending what people are doing with their time they can become more or less boring. Time to assess the current team.
+
+// You will be provided with an object(staff) containing the staff names as keys, and the department they work in as values.
+
+// Each department has a different boredom assessment score, as follows:
+
+// accounts = 1
+// finance = 2
+// canteen = 10
+// regulation = 3
+// trading = 6
+// change = 6
+// IS = 8
+// retail = 5
+// cleaning = 4
+// pissing about = 25
+
 const boredomScore = (staff) => {};
