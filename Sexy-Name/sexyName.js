5951adbd0794486412f743eae93aa761bd138f47
Carlos Pires <extraterra1@gmail.com>
2024-08-07 11:37:56 +0100

    solve sexy name

--- Sexy-Name/sexyName.js
+++ Sexy-Name/sexyName.js
@@ -13,9 +13,47 @@
 // 301 <= score <= 599:  'VERY SEXY'
 //        score >= 600:  'THE ULTIMATE SEXIEST'
 
-const sexyName = (name) => {};
+const sexyName = (name) => {
+  const scores = {
+    A: 100,
+    B: 14,
+    C: 9,
+    D: 28,
+    E: 145,
+    F: 12,
+    G: 3,
+    H: 10,
+    I: 200,
+    J: 100,
+    K: 114,
+    L: 100,
+    M: 25,
+    N: 450,
+    O: 80,
+    P: 2,
+    Q: 12,
+    R: 400,
+    S: 113,
+    T: 405,
+    U: 11,
+    V: 10,
+    W: 10,
+    X: 3,
+    Y: 210,
+    Z: 23
+  };
+  const sexyness = name
+    .toUpperCase()
+    .split('')
+    .reduce((acc, val) => (scores[val] ? acc + scores[val] : acc), 0);
+
+  console.log(sexyness);
+  return sexyness >= 600 ? 'THE ULTIMATE SEXIEST' : sexyness >= 301 ? 'VERY SEXY' : sexyness >= 61 ? 'PRETTY SEXY' : 'NOT TOO SEXY';
+};
 
 console.log(sexyName('GUV'));
 console.log(sexyName('BOB'));
 console.log(sexyName('YOU'));
 console.log(sexyName('ROBBY'));
+
+console.log(sexyName('j eVrG'));
