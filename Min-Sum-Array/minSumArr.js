0897095f7915c8dd58815564986e41cd34c1cf15
Carlos Pires <extraterra1@gmail.com>
2024-08-19 14:29:04 +0100

    solve min sum arr

--- Min-Sum-Array/minSumArr.js
+++ Min-Sum-Array/minSumArr.js
@@ -9,7 +9,14 @@
 // Explanation:
 // The minimum sum obtained from summing each two integers product ,  5*2 + 3*4 = 22
 
-const minSum = (arr) => {};
+const minSum = (arr) => {
+  let sum = 0;
+  arr.sort((a, b) => b - a);
+  while (arr.length > 0) {
+    sum += arr.shift() * arr.pop();
+  }
+  return sum;
+};
 
 console.log(minSum([5, 4, 2, 3]));
 console.log(minSum([12, 6, 10, 26, 3, 24]));
