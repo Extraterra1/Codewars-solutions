82ec5e215da0ba529e602f05b4ab116afdc34430
Carlos Pires <extraterra1@gmail.com>
2024-08-21 15:31:55 +0100

    count digit examples

--- Count-Digit/countDigit.js
+++ Count-Digit/countDigit.js
@@ -25,3 +25,7 @@ const countDigit = (n, d) => {
 
 console.log(countDigit(10, 1));
 console.log(countDigit(25, 1));
+console.log(nbDig(5750, 0));
+console.log(nbDig(11011, 2));
+console.log(nbDig(12224, 8));
+console.log(nbDig(11549, 1));
