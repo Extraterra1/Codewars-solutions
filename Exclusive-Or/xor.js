4b2d957d26aa169daaea7a8b3f918c79355fcf5b
Carlos Pires <extraterra1@gmail.com>
2024-08-28 11:10:35 +0100

    solve exclusive or

--- Exclusive-Or/xor.js
+++ Exclusive-Or/xor.js
@@ -9,7 +9,9 @@
 // Task
 // Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
 
-const xor = (a, b) => {};
+const xor = (a, b) => {
+  return a + b < 2 && (a || b);
+};
 
 console.log(xor(false, false));
 console.log(xor(true, true));
