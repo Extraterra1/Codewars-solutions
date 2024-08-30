6ff327e2f25cce9ac5172c476abf49b6440f83c9
Carlos Pires <extraterra1@gmail.com>
2024-08-30 10:03:25 +0100

    reverse words string examples

--- Reverse-Words/Reverse-Words-String/reverseWords.js
+++ Reverse-Words/Reverse-Words-String/reverseWords.js
@@ -13,3 +13,6 @@ const reverse = (str) => {};
 console.log(reverse('Hello World'));
 console.log(reverse('I am an expert at this'));
 console.log(reverse('This is so easy'));
+console.log(reverse('no one cares'));
+console.log(reverse(''));
+console.log(reverse('CodeWars'));
