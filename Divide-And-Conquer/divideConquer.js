03fe9023615201e5938759c8a101328b40330354
Carlos Pires <extraterra1@gmail.com>
2024-09-06 11:17:25 +0100

    divide and conquer examples

--- Divide-And-Conquer/divideConquer.js
+++ Divide-And-Conquer/divideConquer.js
@@ -6,3 +6,5 @@ const divideConquer = (arr) => {};
 
 console.log(divideConquer([9, 3, '7', '3']), 'should equal 2');
 console.log(divideConquer(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 'should equal 14');
+console.log(divideConquer(['3', 6, 6, 0, '5', 8, 5, '6', 2, '0']), 'should equal 13');
+console.log(divideConquer([1, 2, 3, 4]), 'should equal 10');
