aa8dddb407fc32f9bb8a69f4b04d1d39807de068
Carlos Pires <extraterra1@gmail.com>
2024-09-09 15:46:52 +0100

    solve welcome

--- Welcome/welcome.js
+++ Welcome/welcome.js
@@ -4,7 +4,29 @@
 // Think of a way to store the languages as a database. The languages are listed below so you can copy and paste!
 // Write a 'welcome' function that takes a parameter 'language', with a type String, and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
 
-const welcome = (language) => {};
+const welcome = (language) => {
+  const greetings = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  };
+
+  return greetings[language] || 'Welcome';
+};
 
 console.log(welcome('english'));
 console.log(welcome('dutch'));
