852fd2701cb1e3c545ca4691c9bc7e0d40aac009
Carlos Pires <extraterra1@gmail.com>
2024-09-09 15:44:41 +0100

    welcome examples

--- Welcome/welcome.js
+++ Welcome/welcome.js
@@ -5,3 +5,5 @@
 // Write a 'welcome' function that takes a parameter 'language', with a type String, and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
 
 const welcome = (language) => {};
+
+console.log(welcome('english'));
