93ab843eccea36eb7e228efd9a893f0160119dac
Carlos Pires <extraterra1@gmail.com>
2024-09-14 15:21:27 +0100

    solve cuboid volume

--- Cuboid-Volume/cuboidVolume.js
+++ Cuboid-Volume/cuboidVolume.js
@@ -1,6 +1,8 @@
 // Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
 
-const volume = (length, width, height) => {};
+const volume = (length, width, height) => {
+  return length * width * height;
+};
 
 console.log(volume(1, 2, 2));
 console.log(volume(6, 2, 5));
