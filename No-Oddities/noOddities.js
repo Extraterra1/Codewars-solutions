94574215b0f7400829ba2edce802c037a258a402
Carlos Pires <extraterra1@gmail.com>
2024-09-29 21:43:11 +0100

    solve no oddities

--- No-Oddities/noOddities.js
+++ No-Oddities/noOddities.js
@@ -2,7 +2,9 @@
 
 // All values in the array will be integers. Return the good values in the order they are given.
 
-const noOddities = (arr) => {};
+const noOddities = (arr) => {
+  return arr.filter((e) => !(e % 2));
+};
 
 console.log(noOddities([1, 2, 3]));
 console.log(noOddities([0, 1]));
