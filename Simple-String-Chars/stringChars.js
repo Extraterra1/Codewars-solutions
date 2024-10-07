bbbfa3a134c2b51d429e2f0c3fcd44f2565f771a
Carlos Pires <extraterra1@gmail.com>
2024-10-07 16:20:36 +0100

    simple string chars examples

--- Simple-String-Chars/stringChars.js
+++ Simple-String-Chars/stringChars.js
@@ -10,4 +10,10 @@
 const solve = (s) => {};
 
 console.log(solve("*'&ABCDabcde12345"));
-console.log(solve(''));
+// console.log(solve(''));
+console.log(solve('Codewars@codewars123.com'), [1, 18, 3, 2]);
+console.log(solve('bgA5<1d-tOwUZTS8yQ'), [7, 6, 3, 2]);
+console.log(solve('P*K4%>mQUDaG$h=cx2?.Czt7!Zn16p@5H'), [9, 9, 6, 9]);
+console.log(solve("RYT'>s&gO-.CM9AKeH?,5317tWGpS<*x2ukXZD"), [15, 8, 6, 9]);
+console.log(solve('$Cnl)Sr<7bBW-&qLHI!mY41ODe'), [10, 7, 3, 6]);
+console.log(solve('@mw>0=QD-iAx!rp9TaG?o&M%l$34L.nbft'), [7, 13, 4, 10]);
