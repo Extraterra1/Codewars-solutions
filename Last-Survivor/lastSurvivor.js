fc98ee3a635c22e2798960c992e6a925555e3a2c
Carlos Pires <extraterra1@gmail.com>
2024-10-09 14:38:25 +0100

    initial contamination

--- Last-Survivor/lastSurvivor.js
+++ Last-Survivor/lastSurvivor.js
@@ -1,1 +1,8 @@
-const lastSurvivor = (str, coords) => {};
+const lastSurvivor = (str, coords) => {
+  for (const i of coords) {
+    str = str.replace(str[i], '');
+  }
+  return str;
+};
+
+console.log(lastSurvivor('zbk', [0, 1]));
