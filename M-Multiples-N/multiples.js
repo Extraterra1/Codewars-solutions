60f0736113911a857f801d7e32b5a2296f0b432a
Carlos Pires <extraterra1@gmail.com>
2024-10-18 16:00:58 +0100

    m multiples of n examples

--- M-Multiples-N/multiples.js
+++ M-Multiples-N/multiples.js
@@ -1,3 +1,8 @@
 // Implement a function that takes two numbers m and n and returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
 
 const multiples = (m, n) => {};
+
+console.log(multiples(3, 5));
+console.log(multiples(6, 1));
+console.log(multiples(23, 2));
+console.log(multiples(43, 5));
