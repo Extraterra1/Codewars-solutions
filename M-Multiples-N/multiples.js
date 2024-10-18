dc2b7986e78093ea78675338c306a4113f9adf4c
Carlos Pires <extraterra1@gmail.com>
2024-10-18 15:51:47 +0100

    m multiples of n explanation

--- M-Multiples-N/multiples.js
+++ M-Multiples-N/multiples.js
@@ -1,1 +1,3 @@
+// Implement a function that takes two numbers m and n and returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
+
 const multiples = (m, n) => {};
