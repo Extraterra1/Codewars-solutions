6d6386ea1758b048d7f59db270dba19faedb8423
Carlos Pires <extraterra1@gmail.com>
2024-10-18 16:01:10 +0100

    solve m multiples of n

--- M-Multiples-N/multiples.js
+++ M-Multiples-N/multiples.js
@@ -1,6 +1,8 @@
 // Implement a function that takes two numbers m and n and returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
 
-const multiples = (m, n) => {};
+const multiples = (m, n) => {
+  return [...Array(m)].map((_, i) => (i + 1) * n);
+};
 
 console.log(multiples(3, 5));
 console.log(multiples(6, 1));
