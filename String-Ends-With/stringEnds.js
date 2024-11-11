f64e661cdbab23ff3efaf5a1b8628d7285f1a0bb
Carlos Pires <extraterra1@gmail.com>
2024-11-11 22:57:58 +0000

    string ends with examples

--- String-Ends-With/stringEnds.js
+++ String-Ends-With/stringEnds.js
@@ -1,1 +1,6 @@
 const stringEnds = (str, end) => {};
+
+console.log(stringEnds('abcdeee', ''));
+console.log(stringEnds('abcdeee', 'ee'));
+console.log(stringEnds('abc', 'bc'));
+console.log(stringEnds('abc', 'd'));
