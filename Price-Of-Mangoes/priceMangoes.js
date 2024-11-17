a174991ca20288f685c5c3ea6adab440f5171591
Carlos Pires <extraterra1@gmail.com>
2024-11-17 22:42:28 +0000

    price of mangoes examples

--- Price-Of-Mangoes/priceMangoes.js
+++ Price-Of-Mangoes/priceMangoes.js
@@ -1,3 +1,8 @@
 // There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
 
-const priceMangoes = (price, quantity) => {};
+const priceMangoes = (quantity, price) => {};
+
+console.log(priceMangoes(2, 3));
+console.log(priceMangoes(3, 3));
+console.log(priceMangoes(5, 3));
+console.log(priceMangoes(9, 5));
