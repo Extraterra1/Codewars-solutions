7f5ce5836d62fd3a66834edc3d1e28e0e58c12e6
Carlos Pires <extraterra1@gmail.com>
2024-11-22 23:34:27 +0000

    solve price of mangoes

--- Price-Of-Mangoes/priceMangoes.js
+++ Price-Of-Mangoes/priceMangoes.js
@@ -1,6 +1,8 @@
 // There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
 
-const priceMangoes = (quantity, price) => {};
+const priceMangoes = (quantity, price) => {
+  return (quantity - Math.floor(quantity / 3)) * price;
+};
 
 console.log(priceMangoes(2, 3));
 console.log(priceMangoes(3, 3));
